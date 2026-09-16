a0e2538daf6462b68b74c75490997a8f8fcbc43c
dortiz2025 <dortiz2025@inf.udec.cl>
2026-09-16 16:57:10 -0300

    feat: se añaden los primeros comandos internos cd y exit.

---
+++ src/comandos_internos.c
@@ -0,0 +1,17 @@
+#include <comandos_internos.h>
+
+//Comando cd
+static void ejecutar_cd(char **args) {
+    char *dir = args[1] != NULL ? args[1] : getenv("HOME");//Verifica argumento luego de cd
+    //Si no está definido "HOME" lanza error.
+    if (dir == NULL) fprintf(stderr, "miShell: cd: HOME no definido\n");
+    //Se intenta cambiar de directorio, si no es posible, lanza error.
+    else if (chdir(dir) != 0) perror("miShell: cd");
+}
+
+//Comando exit
+static void ejecutar_exit(char **args) {
+    //Antes de cerrar la shell, se guarda estado si el usuario lo ingresó. Por defecto 0.
+    int status = args[1] != NULL ? atoi(args[1]) : 0;
+    exit(status);
+}
