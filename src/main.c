35c854b07c3371d20bb71f78d80b79b0894a6203
dortiz2025 <dortiz2025@inf.udec.cl>
2026-09-16 14:55:10 -0300

    fix: se añade verificación de linea vacía en el ciclo principal de la shell.

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ int main(int, char**){
         imprimir_prompt(); //imprime prompt en consola, hay que cambiarlo por una función que cumpla los requisitos
         linea = leer_linea(); //lee la linea desde consola
         tokens = parsear_linea(linea); //parsea la linea en argumentos dentro de un arreglo
+        if (tokens[0] == NULL) continue;//Se verifica si no se escribió nada.
         status = lanzar_proceso(tokens); //ejecuta el comando ingresado
         
         free(linea); //libera memoria de linea y tokens en cada iteración
