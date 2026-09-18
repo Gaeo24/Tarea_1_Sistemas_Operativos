ead25674e14a0abb42f3ee99eecf2a27ac68d03d
dortiz2025 <dortiz2025@inf.udec.cl>
2026-09-18 09:42:35 -0300

    feat: se añade struct que concentra datos de la shell para utilizarlo posteriormente al salir de la shell, modificando estados desde comandos.

--- include/shell.h
+++ include/shell.h
@@ -1,2 +1,14 @@
+#ifndef SHEL_H
+#define SHEL_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
+//Struct que concentra datos de la shell
+typedef struct {
+    int running;          //Define si el programa continua o no.
+    int exit_status;      //Almacena el estado de salida si se usó exit.
+    // ListaJobs *jobs; //Se añadirá después...
+} ShellState;
+
+#endif
