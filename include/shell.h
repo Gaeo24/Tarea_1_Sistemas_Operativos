3368c846ba9fe542ccc439a58de7bf2c933b71de
dortiz2025 <dortiz2025@inf.udec.cl>
2026-09-18 15:20:25 -0300

    feat: se añade struct para poder concentrar datos asociados a un job.

--- include/shell.h
+++ include/shell.h
@@ -4,11 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Struct que concentra los datos de un job (proceso o grupo de procesos asociados a la shell)
+typedef struct {
+    int id;
+    pid_t pid;
+    char comando[256];
+    int activo;
+} Job;
+
 //Struct que concentra datos de la shell
 typedef struct {
     int running;          //Define si el programa continua o no.
     int exit_status;      //Almacena el estado de salida si se usó exit.
-    // ListaJobs *jobs; //Se añadirá después...
+    Job *jobs;          //
+    int capacidad;      // 
+    int contador_jobs;
 } ShellState;
 
 #endif
