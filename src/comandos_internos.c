f603d61250dbe008c2eca3db64c66ad93b354867
dortiz2025 <dortiz2025@inf.udec.cl>
2026-09-26 12:05:11 -0300

    feat: se crea la estructura del comando pmon, su ciclo, y la configuración de las señales para que usen los handlers anteriormente implementados.

--- src/comandos_internos.c
+++ src/comandos_internos.c
@@ -3,19 +3,19 @@
 #include <shell.h>
 
 // Banderas globales exclusivas para pmon
-volatile sig_atomic_t pmon_corriendo = 1;
+volatile sig_atomic_t running_pmon = 1;
 volatile sig_atomic_t pmon_imprimir = 1;
 
-// Manejador que se activa cada vez que la alarma suena (pmon)
+// Manejador que se activa cada vez que la alarma suena (pmon).
 static void manejador_pmon_alarma(int sig) {
     (void)sig;
-    pmon_imprimir = 1; // Avisamos que hay que redibujar la tabla
+    pmon_imprimir = 1; //Avisamos que hay que redibujar la tabla.
 }
 
 // Manejador que se activa al presionar Ctrl+C
 static void manejador_pmon_salir(int sig) {
     (void)sig;
-    pmon_corriendo = 0; // Avisamos que hay que romper el ciclo principal
+    running_pmon = 0; //Avisamos que hay que terminar el ciclo.
 }
 
 //Comando cd
@@ -79,6 +79,59 @@ static void ejecutar_jobs(char **args, ShellState *shellState) {
 
     //Se desbloquean las señales.
     sigprocmask(SIG_SETMASK, &mascara_original, NULL);
+}
+
+static void ejecutar_pmon(char **args, ShellState *shellState) {
+    //Si no se entregan segundos, por defecto 2.
+    int segundos = 2;
+    if (args[1] != NULL) {
+        segundos = atoi(args[1]);
+        if (segundos <= 0) segundos = 2;
+    }
+
+    //Sobreecribimos señales para manejarlas.
+    struct sigaction sa_alrm, sa_int, sa_old_alrm, sa_old_int;
+
+    //Configuramos la alarma.
+    sa_alrm.sa_handler = manejador_pmon_alarma;
+    sigemptyset(&sa_alrm.sa_mask);
+    sa_alrm.sa_flags = 0;
+
+    //Configuramos Ctrl+C para terminar.
+    sa_int.sa_handler = manejador_pmon_salir;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = 0;
+
+    //Añadimos nueva configuración y guardamos las originales.
+    sigaction(SIGALRM, &sa_alrm, &sa_old_alrm);
+    sigaction(SIGINT, &sa_int, &sa_old_int);
+
+    //---Ciclo del comando---
+
+    running_pmon = 1;
+    pmon_imprimir = 1;
+
+    while (running_pmon) {
+        if (pmon_imprimir) {
+            pmon_imprimir = 0; //En 0 para que no imprima enseguida.
+
+            printf("\nRefrescando pmon cada %d segundos... (Ctrl+C para salir)\n", segundos);
+            printf("PID\t| COMANDO\t| ESTADO\t| %%CPU\t| RSS (KB)\n");
+
+            //Configuramos alarma para poder actualizar según la variable segundos.
+            alarm(segundos);
+        }
+
+        pause(); 
+    }
+
+    //Si se sale del ciclo...
+    alarm(0); //Cancelamos cualquier alarma pendiente.
+    printf("\nSaliendo del monitor pmon...\n");
+
+    //Volvemos a la configuración original de la shell.
+    sigaction(SIGALRM, &sa_old_alrm, NULL);
+    sigaction(SIGINT, &sa_old_int, NULL);
 }
 
 //Estructura para poder asociar el nombre de un comando (ej: exit) 
